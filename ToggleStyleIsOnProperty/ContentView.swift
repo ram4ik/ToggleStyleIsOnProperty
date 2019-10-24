911f93f5dd6f053d83d572e86abc21690ba0f7b4
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-24 19:37:03 +0300

    init

--- ToggleStyleIsOnProperty/ContentView.swift
+++ ToggleStyleIsOnProperty/ContentView.swift
@@ -10,12 +10,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        ToggleStyle_isOn()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ToggleStyle_isOn: View {
+    @State private var toggleState = true
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Toggle Style").font(.largeTitle)
+            Text("Is On").font(.title).foregroundColor(.gray)
+            Text("You can say the isOn property is the main component of the Toggle. You can customize a toggle to represent this on/off, true/false state.")
+                .frame(maxWidth: .infinity)
+                .font(.title).padding()
+                .background(Color.purple)
+                .layoutPriority(1.0)
+                .foregroundColor(.white)
+            
+            Toggle("", isOn: $toggleState).padding()
+            Toggle("", isOn: $toggleState).padding()
+        }
     }
 }
